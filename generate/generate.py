b8be5ecb023b3cf738c72ddb00947ea849289cd5
PJ Reiniger <pj.reiniger@gmail.com>
2024-04-01 00:21:32 -0400

    AHHH

--- generate/generate.py
+++ generate/generate.py
@@ -4,11 +4,13 @@ from bazelrio_gentool.generate_styleguide_rule import (
 from bazelrio_gentool.clean_existing_version import clean_existing_version
 from get_wpiformat_group import get_wpiformat_group
 from bazelrio_gentool.cli import add_generic_cli, GenericCliArgs
+from bazelrio_gentool.utils import render_templates
 import os
 import argparse
 from bazelrio_gentool.generate_module_project_files import (
     create_default_mandatory_settings,
 )
+from bazelrio_gentool.generate_shared_files import get_bazel_dependencies
 
 
 def main():
@@ -35,6 +37,22 @@ def main():
 
     with open(os.path.join(REPO_DIR, "requirements.txt"), "w") as f:
         f.write(f"wpiformat=={group.version}\n")
+
+        
+    template_files = [
+        "MODULE.bazel",
+    ]
+
+    render_templates(
+        template_files,
+        REPO_DIR,
+        os.path.join(SCRIPT_DIR),
+        group=group,
+        # repos=repos,
+        # libraries=libraries,
+        bazel_dependencies=get_bazel_dependencies(),
+        # mandatory_dependencies=mandatory_dependencies,
+    )
 
 
 if __name__ == "__main__":
